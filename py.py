e3887db91b41e6927499d1c6cbc53ee1822d1da3
gauravkisanmane1 <gauravmane000@gmail.com>
2022-11-24 16:38:29 +0530

    success

--- py.py
+++ py.py
@@ -1,1 +1,16 @@
-list=[1,2,3,4,5,6,7,8,9]
+# import pandas as pd
+# from datetime import time
+# from distutils.cmd import Command as DistutilsCommand
+# df = pd.read_excel('wordcraft.xlsx')
+# #Display the first 10 rows
+# result = df.head(10)
+# print("First 10 rows of the DataFrame:")
+# print(result)
+
+
+a=[10,20,10,50,40,30,30]
+d=[]
+for ele in a:
+    if a.count(ele) >1 and ele not in d:
+        d.append(ele)
+print(d)    
